b95faa9eb13571d083d97fd3e251f618fe8ed1a1
andresamarilis <andybass922@gmail.com>
2021-11-24 22:50:26 +1100

    api test

---
+++ cypress/integration/1-getting-started/tests.spec.js
@@ -0,0 +1,14 @@
+describe('Mocha’s interface', () => {
+    context('it provides a way to keep tests easier to read and organized.', () => {
+      it('This is your test case', () => {
+        expect(true).to.eq(true)
+      });
+    });
+  });
+
+
+
+
+//   ./node_modules/.bin/cypress run
+//   ./node_modules/.bin/cypress open
+// https://dev.to/murillowelsi/api-testing-with-cypress-part-1-5coe
